8039748ebb65ee9e7c13908121f2b845acee9f29
Miguel Panelo <miguelrpanelo@gmail.com>
2018-02-21 01:04:31 +0800

    Implemented Search UI

--- app/src/main/java/io/gumil/cities/view/list/CityListFragment.java
+++ app/src/main/java/io/gumil/cities/view/list/CityListFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
@@ -30,6 +33,18 @@ public class CityListFragment extends Fragment implements CityView {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_list, menu);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -44,6 +59,7 @@ public class CityListFragment extends Fragment implements CityView {
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
 
         if (getActivity() != null && getActivity().getApplication() instanceof CityApplication) {
             CityListPresenter presenter = new CityListPresenter(((CityApplication) getActivity().getApplication()).getRepository(), this);
