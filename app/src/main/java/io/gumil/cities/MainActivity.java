74d6838741feb8f1c2bcf62b6caf0392233f84e4
Miguel Panelo <miguelrpanelo@gmail.com>
2018-02-21 02:31:13 +0800

    Added goTo and showToolbar methods to MainActivity
    Added transition to fragment change

--- app/src/main/java/io/gumil/cities/MainActivity.java
+++ app/src/main/java/io/gumil/cities/MainActivity.java
@@ -1,6 +1,8 @@
 package io.gumil.cities;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import io.gumil.cities.view.list.CityListFragment;
@@ -12,11 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CityListFragment fragment = CityListFragment.newInstance();
-
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.fragmentContainer, fragment, CityListFragment.class.getSimpleName())
-                .commit();
+        goTo(CityListFragment.newInstance(), false);
     }
 
     @Override
@@ -25,6 +23,33 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().popBackStack();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    public void goTo(Fragment fragment, boolean addToBackStack) {
+        String tag = fragment.getClass().getSimpleName();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction()
+                .setCustomAnimations(
+                        R.anim.slide_in_from_right,
+                        R.anim.slide_out_to_left,
+                        R.anim.slide_in_from_left,
+                        R.anim.slide_out_to_right
+                )
+                .add(R.id.fragmentContainer, fragment, tag);
+
+        if (addToBackStack) {
+            transaction.addToBackStack(tag);
+        }
+        transaction.commit();
+    }
+
+    public void showToolbar(boolean show) {
+        if (getSupportActionBar() != null) {
+            if (show) {
+                getSupportActionBar().show();
+            } else {
+                getSupportActionBar().hide();
+            }
         }
     }
 }
