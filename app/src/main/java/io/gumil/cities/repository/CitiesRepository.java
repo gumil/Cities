4a1236e7d33cb833b53534ca818802cde8583cf3
Miguel Panelo <miguelrpanelo@gmail.com>
2018-02-21 22:10:08 +0800

    Added comment explaining the strategy in getting the list of cities

--- app/src/main/java/io/gumil/cities/repository/CitiesRepository.java
+++ app/src/main/java/io/gumil/cities/repository/CitiesRepository.java
@@ -25,6 +25,19 @@ public class CitiesRepository {
 
     /**
      * Lazily gets the list of cities
+     *
+     * Steps:
+     * 1. Read cities.json by line
+     * 2. If line is valid parse to line using Gson
+     * 3. Add City into TreeMap using city name as key
+     *
+     * Since the json file is formatted where each line can be parsed to a city, the list of cities
+     * is processed by getting every line since it's safer to process the cities.json
+     * file that way. Reading in by buffer might cause OutOfMemoryError. Also reading by chunks
+     * can result to malformed json and might need to combine from different chunks.
+     *
+     * Using TreeMap, it can eliminate duplicates and sort the map when inserting.
+     * Searching is also easier since it can get a submap based on prefix.
      */
     @Nullable
     public List<City> getCities() throws IOException {
